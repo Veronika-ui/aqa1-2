b83ddd550dc66bba2da0c115f801010f57e49e98
Veronika <n.kanchura.dodo@gmail.com>
2021-05-14 19:30:23 +0400

    add art

--- rest/src/test/java/ru/netology/rest/MobileBankApiTestV2.java
+++ rest/src/test/java/ru/netology/rest/MobileBankApiTestV2.java
@@ -15,11 +15,11 @@ class MobileBankApiTestV2 {
       // Выполняемые действия
       .when()
           .get("/demo/accounts")
-
+      // Проверки
       .then()
           .statusCode(200)
-           .header("Content-Type", "application/json; charset=UTF-8")
-
+          .header("Content-Type", "application/json; charset=UTF-8")
+          // специализированные проверки - лучше
           .contentType(ContentType.JSON)
       ;
     }
